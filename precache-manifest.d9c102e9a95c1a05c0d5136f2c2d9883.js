8c77230953cee62bd3a945e0623037a0bde69707
Julian Süss <julian.suess@abusix.com>
2020-07-08 23:15:58 +0200

    Updates

---
+++ precache-manifest.d9c102e9a95c1a05c0d5136f2c2d9883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4b03a8a4dac1d9e8c7dccf8184feed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fd5151e4347bd5b8d7a",
+    "url": "/static/css/main.df10ca80.chunk.css"
+  },
+  {
+    "revision": "0649671de63fd5aaaf58",
+    "url": "/static/js/2.12cb2d19.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.12cb2d19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd5151e4347bd5b8d7a",
+    "url": "/static/js/main.7535f82a.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
